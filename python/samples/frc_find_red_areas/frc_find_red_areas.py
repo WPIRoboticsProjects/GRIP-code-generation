0edb96191139765bbdd06698e99020c7495cf969
drbobbob <dr.bob.bob.bob@gmail.com>
2017-01-22 19:43:51 -0600

    Fix network tables values for python sample (#8)

--- python/samples/frc_find_red_areas/frc_find_red_areas.py
+++ python/samples/frc_find_red_areas/frc_find_red_areas.py
@@ -24,9 +24,9 @@ def extra_processing(pipeline):
     for contour in pipeline.filter_contours_output:
         x, y, w, h = cv2.boundingRect(contour)
         center_x_positions.append(x + w / 2)  # X and Y are coordinates of the top-left corner of the bounding box
-        center_y_positions.append(y + h / w)
+        center_y_positions.append(y + h / 2)
         widths.append(w)
-        heights.append(y)
+        heights.append(h)
 
     # Publish to the '/vision/red_areas' network table
     table = NetworkTables.getTable('/vision/red_areas')
